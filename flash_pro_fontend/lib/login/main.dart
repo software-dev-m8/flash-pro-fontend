35a7a92d9a1e04afbe3a82d47d3206b7d3c191a4
Pornthep Thammawong <106590971+Kfirm1208@users.noreply.github.com>
2024-10-26 01:08:52 +0700

    fix

--- flash_pro_fontend/lib/login/main.dart
+++ flash_pro_fontend/lib/login/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flash_pro_fontend/login/first.dart';
-import 'config/theme.dart';
-import 'restaurant_management.dart';
+// import 'config/theme.dart';
+// import 'restaurant_management.dart';
 
 class Main extends StatelessWidget {
   const Main({super.key});
